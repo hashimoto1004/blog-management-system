aaff241950585a4efc02742c250682d6171b4de6
hashimoto <shinya.h.1004@gmail.com>
2024-08-10 12:48:24 +0900

    ブログのDDLを作成

--- src/main/resources/db/create_blogs_table.sql
+++ src/main/resources/db/create_blogs_table.sql
@@ -1,2 +1,12 @@
 DROP TABLE IF EXISTS blogs CASCADE;
 
+-- Blogテーブルの作成
+CREATE TABLE blogs (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP,
+);
+
